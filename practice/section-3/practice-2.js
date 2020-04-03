459403941911a80ba7aa6f23bbb7ae592ab38e8d
Peng Chen <pchen@thoughtworks.com>
2020-04-03 11:28:39 +0800

    Add section-3 practice-2 implementation

--- practice/section-3/practice-2.js
+++ practice/section-3/practice-2.js
@@ -1,5 +1,13 @@
 'use strict';
 
+const MATCH_NUMBER = 3;
+
 function createUpdatedCollection(collectionA, objectB) {
-  return 'Implement the practice require, and begin changing code in this row';
+  return collectionA.map(a => {
+    let result = objectB.value.find(elementB => a.key == elementB)
+    if(result !== undefined && a.count >= MATCH_NUMBER) {
+      a.count -= Math.floor(a.count / MATCH_NUMBER);
+    }
+    return a;
+  })
 }
