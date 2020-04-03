6a4feadbaf1a1776d706ac29ca8cd53abce8d253
Peng Chen <pchen@thoughtworks.com>
2020-04-03 10:29:32 +0800

    Add section-1 practice-1 implementation

--- practice/section-1/practice-1.js
+++ practice/section-1/practice-1.js
@@ -1,5 +1,12 @@
 'use strict';
 
 function collectSameElements(collectionA, collectionB) {
-  return 'Implement the practice require, and begin changing code in this row';
+  let resultCollection = [];
+  collectionA.forEach(elementA => {
+    let result = collectionB.find(elementB => elementA == elementB)
+    if(result !== undefined) {
+      resultCollection.push(result);
+    }
+  });
+  return resultCollection;
 }
