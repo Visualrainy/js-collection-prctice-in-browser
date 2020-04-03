3cfecc6dc0535dae00532b23ac0a8aceb25037b1
Peng Chen <pchen@thoughtworks.com>
2020-04-03 12:24:26 +0800

    Add section-3 practice-4 implementation

--- practice/section-3/practice-4.js
+++ practice/section-3/practice-4.js
@@ -1,5 +1,34 @@
 'use strict';
 
+const MATCH_NUMBER = 3;
 function createUpdatedCollection(collectionA, objectB) {
-  return 'Implement the practice require, and begin changing code in this row';
+  let filterCollection = collectionA.filter((item, index, array) => array.indexOf(item, 0) === index)
+  
+  let mapCollection = filterCollection.map(elementA => {
+    let count = collectionA.filter(item => item === elementA).length;  
+    if(elementA.indexOf('-') != -1) {
+      let specialElement = elementA.split('-');
+      return {key: specialElement[0], count: specialElement[1] * count}
+    }
+
+    return {key: elementA, count: count}
+  })
+  mapCollection.filter((item, index, array) => {
+
+  })  
+  
+  mapCollection.map(item => item.key).forEach((item, index, array) => {
+    if(array.indexOf(item, 0) != index) {
+      mapCollection[array.indexOf(item, 0)].count += mapCollection[index].count; 
+      mapCollection.splice(index, 1)
+    }
+  })
+
+  return mapCollection.map(a => {
+    let result = objectB.value.find(elementB => a.key == elementB)
+    if(result !== undefined && a.count >= MATCH_NUMBER) {
+      a.count -= Math.floor(a.count / MATCH_NUMBER);
+    }
+    return a;
+  })
 }
